022399d69e747fa6ff457110b227e2c5b17e6646
nxc5929 <nxc5929@rit.edu>
2019-11-23 11:21:24 -0500

    Added Drives Motors

--- src/main/java/frc/robot/IO.java
+++ src/main/java/frc/robot/IO.java
@@ -6,6 +6,9 @@ package frc.robot;
  */
 public final class IO {
 
-	
+	public static final int RIGHT_MOTOR_1		= 0;
+	public static final int RIGHT_MOTOR_2		= 1;
+	public static final int LEFT_MOTOR_1		= 2;
+	public static final int LEFT_MOTOR_2		= 3;
 	
 }

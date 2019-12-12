d4388b1cba9ee12bc78fe14742602f3e683b439c
agarcia22 <agarcia22@THS-430-24.websterschools.net>
2019-12-11 19:18:08 -0500

    fixed code by calling get raw-axis method from genericHID base class

--- src/main/java/frc/controllers/Axis.java
+++ src/main/java/frc/controllers/Axis.java
@@ -1,33 +1,36 @@
 package frc.controllers;
 
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Joystick.AxisType;
+//import edu.wpi.first.wpilibj.Joystick.AxisType;
 
 /**
- * Getter for Joystick Axis
- */
+* Getter for Joystick Axis
+*/
 public class Axis {
 	
-	private Joystick joystick;
+private Joystick joystick;
 	private int  axis;
 	private boolean invert;
 	
-	public Axis(Joystick joystick, int axis, boolean invert) {
+public Axis(Joystick joystick, int axis, boolean invert) {
 		this.joystick = joystick;
 		this.axis = axis;
 		this.invert = invert;
 	}
-
 	/**
 	 * Add Deadband to remove false movement
 	 * @return value of specified axis
 	 */
-	public double get() 
+	public double get()
 	{
-		if (invert == true) 
-			return -axis;
-		else 
-			return axis;
+		
+		if (true  == this.invert)
+		{
+			return -this.joystick.getRawAxis(this.axis);
+		}
+		else
+		{
+			return this.joystick.getRawAxis(this.axis);
+		}
 	}
-	
 }

09b2ee1d11f7199749f8c13d566b333e9a4ff13e
nxc5929 <nxc5929@rit.edu>
2020-01-07 11:21:28 -0500

    Added Motor Sensors

--- src/main/java/frc/robot/IO.java
+++ src/main/java/frc/robot/IO.java
@@ -5,7 +5,10 @@ package frc.robot;
  * All Sensor input IO
  */
 public final class IO {
-
 	
+	public static final int RIGHT_ENCODER_A = 10;
+	public static final int RIGHT_ENCODER_B = 11;
+	public static final int LEFT_ENCODER_A = 12;
+	public static final int LEFT_ENCODER_B = 13;
 	
 }

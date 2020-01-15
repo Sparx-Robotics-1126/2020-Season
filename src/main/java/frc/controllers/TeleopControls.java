959bb055a83e855235f177f7c19461ba12cfbcab
skyler <skylerssparxemail@gmail.com>
2020-01-15 18:41:20 -0500

    should be nothing

--- src/main/java/frc/controllers/TeleopControls.java
+++ src/main/java/frc/controllers/TeleopControls.java
@@ -22,11 +22,11 @@ public class TeleopControls extends Controller {
 
     @Override
     public void execute() {
-    	if(spinButton.get()) {
+		if(spinButton.get()) {
     		drives.startSpin();
     	}
     	if(buttonLeft.get()) {
-    		drives.turnLeft(90);
+			drives.turnLeft(90);
     	}
     } 
 }

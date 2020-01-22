7075e4ca439cc4dae742fe250fddf9ea7f114d57
skylerssparxemail@gmail.com <skylerssparxemail@gmail.com>
2020-01-22 18:46:55 -0500

    using phyics not lookup tabl

--- src/main/java/frc/shooter/MathHelpers.java
+++ src/main/java/frc/shooter/MathHelpers.java
@@ -2,12 +2,15 @@ package frc.shooter;
 
 public class MathHelpers {
 	
+	final static double heightOfPort = 2.4955; //In meters
+	
+
 	public static double getShootOffset(double cameraToRobotAngle, double shootingSpeed, double robotSpeed) {
 		return 0;
 	}
 	
 	public static double getShootingSpeed(double distanceFromTarget) {
-		return 0;
+		return Math.sqrt((distanceFromTarget*distanceFromTarget*9.8)/(2*Math.cos(35)*heightOfPort-Math.tan(35)));
 	}
 
 }

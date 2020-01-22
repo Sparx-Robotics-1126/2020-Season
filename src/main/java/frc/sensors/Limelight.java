3413cf7b5e227d59981a3b9c52a99ad9136f439b
smoulton23 <smoulton23@THS-430-21.websterschools.net>
2020-01-21 19:51:18 -0500

    Finished limelight getangleFromTarget method

--- src/main/java/frc/sensors/Limelight.java
+++ src/main/java/frc/sensors/Limelight.java
@@ -11,12 +11,19 @@ public class Limelight {
 		table = NetworkTableInstance.getDefault().getTable("limelight");
 	}
 	
+	NetworkTableEntry tx = table.getEntry("tx");
+	NetworkTableEntry ty = table.getEntry("ty");
+	
+	
 	public double getDistanceFromTarget() {
 		return 0;
 	}
 	
 	public double getAngleFromTarget() {
-		return 0;
+		double x = tx.getDouble(0.0);
+		double y = ty.getDouble(0.0);
+		return x + y;
+		//System.out.println("Offset from target (x, y): " + Double.valueOf(x) + ", " + Double.valueOf(y)); //for testing
 	}
 	
 	public void enable(boolean enable) {

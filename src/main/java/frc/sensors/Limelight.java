4ef2a4917725033407798fdcd8b1648806e244d1
ssypnier22 <ssypnier22@THS-430-23.websterschools.net>
2020-01-25 14:12:57 -0500

    Added code into the getDistanceFromTarget method

--- src/main/java/frc/sensors/Limelight.java
+++ src/main/java/frc/sensors/Limelight.java
@@ -1,9 +1,17 @@
 package frc.sensors;
 
+import java.lang.Math;
+
 public class Limelight {
 	
 	public double getDistanceFromTarget() {
-		return 0;
+		
+		final double a1 = 75;
+		final double a2 = getAngleFromTarget();
+		final double h1 = 36;
+		final double h2 = 98.25;
+		
+		return (h2-h1) / Math.tan(a1+a2);
 	}
 	
 	public double getAngleFromTarget() {

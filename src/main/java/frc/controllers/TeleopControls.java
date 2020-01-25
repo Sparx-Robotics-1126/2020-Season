d30c70a128267fc4b184f346723323db14efdcaf
JTMongeon <jtmongeon@gmail.com>
2020-01-25 10:54:23 -0500

    branch testing successful, have a nice day.

--- src/main/java/frc/controllers/TeleopControls.java
+++ src/main/java/frc/controllers/TeleopControls.java
@@ -26,8 +26,8 @@ public class TeleopControls extends Controller {
         
         Joystick joystick = new Joystick(0);
 
-        axis1 = new Axis(joystick, 0, false);
-        axis2 = new Axis(joystick, 0, false);
+        axis1 = new Axis(joystick, 5, true);
+        axis2 = new Axis(joystick, 1, true);
         
         moveForwardButton = new Button(joystick, 1);
         backwardButton = new Button(joystick, 2);

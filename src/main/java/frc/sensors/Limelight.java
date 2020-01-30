c19a2700906a42913fe8ed3021d9ef43e8f8cbbe
JTMongeon <jtmongeon@gmail.com>
2020-01-29 20:19:00 -0500

    changed call to getAngle to getting data from network table

--- src/main/java/frc/sensors/Limelight.java
+++ src/main/java/frc/sensors/Limelight.java
@@ -2,12 +2,24 @@ package frc.sensors;
 
 import java.lang.Math;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
 public class Limelight {
 	
+	private NetworkTable table;
+	NetworkTableEntry ty;
+	
+	public Limelight() {
+		table = NetworkTableInstance.getDefault().getTable("limelight");
+		ty = table.getEntry("ty");
+	}
+
 	public double getDistanceFromTarget() {
 		
-		final double a1 = 75;
-		final double a2 = getAngleFromTarget();
+		final double a1 = 15;
+		final double a2 = ty.getDouble(0);
 		final double h1 = 36;
 		final double h2 = 98.25;
 		

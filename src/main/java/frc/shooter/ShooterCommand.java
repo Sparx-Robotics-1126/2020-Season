c929899a4158f17ddf98f453c9896f84a2da1946
SkF0023 <skylerssparxemail@gmail.com>
2020-02-03 17:18:05 -0500

    made shooter command to search for target

--- src/main/java/frc/shooter/ShooterCommand.java
+++ src/main/java/frc/shooter/ShooterCommand.java
@@ -7,10 +7,12 @@ public abstract class ShooterCommand {
      * Class containing getters for all sensors needed for this subsystem
      */
     protected ShooterSensorsInterfeace sensors;
+    protected DrivesSensorInterface driveSensors;
 
     //Constructor
     public ShooterCommand(ShooterSensorsInterfeace sensors, DrivesSensorInterface driveSensors){
         this.sensors = sensors;
+        this.driveSensors = driveSensors;
     }
 
     //Will be called by subsystem to execute this command

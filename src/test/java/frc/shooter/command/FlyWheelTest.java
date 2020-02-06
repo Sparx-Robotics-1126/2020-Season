b0c8bebfa55f25b154203e5f68964b25d889547d
Nick <Nick@10.94.37.169>
2020-02-05 19:48:51 -0500

    FlyWheel test returns 0.4

---
+++ src/test/java/frc/shooter/command/FlyWheelTest.java
@@ -0,0 +1,19 @@
+package frc.shooter.command;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import frc.shooter.ShooterCommand;
+import frc.shooter.ShooterOutput;
+
+public class FlyWheelTest {
+	
+	@Test
+	public void TestFlyWheelReturns45() {
+		ShooterCommand command = new TestFlywheel(null, null);
+		ShooterOutput output = command.execute();
+		assertEquals(0.4, output.getOutputValue(), 0.01);
+		
+	}
+}

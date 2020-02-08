3bfdd58ef89bfb1adf60cb46bc94209c30d75ef8
JTMongeon <jtmongeon@gmail.com>
2020-02-08 10:23:08 -0500

    Split branches

--- src/main/java/frc/subsystem/Storage.java
+++ src/main/java/frc/subsystem/Storage.java
@@ -32,7 +32,72 @@ public class Storage extends Subsystem{
 	public boolean isDone() {
 		return storageCommand == null;
 	}
+	package frc.subsystem;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import frc.robot.IO;
+import frc.storage.StorageCommand;
+import frc.storage.StorageOutput;
+import frc.storage.StorageSensorInterface;
+
+public class Storage extends Subsystem{
+
+	private StorageCommand storageCommand;
+	private StorageSensorInterface sensors;
 	
+	private short numOfBallsAquired;
+	
+	private TalonSRX motorMaster;
+	
+	private static final double STORAGE_MAX_VOLTAGE = 12.0;
+	
+	public Storage(StorageSensorInterface storageSensors) {
+		sensors = storageSensors;
+		motorMaster = new TalonSRX(IO.RIGHT_MOTOR_1);
+		TalonSRX motorSlave = new TalonSRX(IO.LEFT_MOTOR_1);
+		motorSlave.setInverted(true);
+		configureMotor(motorMaster, motorSlave);
+		
+		
+		this.sensors = null;
+		
+	}
+	private static void configureMotor(TalonSRX master, TalonSRX slave) {
+		int masterId = master.getDeviceID();
+		master.set(ControlMode.PercentOutput, 0);
+		master.configVoltageCompSaturation(STORAGE_MAX_VOLTAGE);
+		master.enableVoltageCompensation(true);
+		slave.set(ControlMode.Follower, masterId);
+	}
+	
+	@Override
+	void execute() {
+		if(storageCommand != null) {
+			StorageOutput output = storageCommand.execute();
+			numOfBallsAquired = output.getNumOfBallsAquired();
+			//Set Motor Values
+			motorMaster.set(ControlMode.PercentOutput, output.getOutput());
+			if(output.isCommandFinished()) {
+				//TURN OFF MOTORS
+				motorMaster.set(ControlMode.PercentOutput, 0);
+				storageCommand = null;
+			}
+		}
+	}
+
+	@Override
+	public boolean isDone() {
+		return storageCommand == null;
+	}
+	
+	public short getNumberOfBallsAquired() {
+		return numOfBallsAquired;
+	}
+
+}
+
 	public short getNumberOfBallsAquired() {
 		return numOfBallsAquired;
 	}

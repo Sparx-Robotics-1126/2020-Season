aeea34dc2a7501e003754fddccac3d5270e8c52c
Nick <Nick@10.94.37.169>
2020-02-10 17:10:54 -0500

    returns 0 if AcqCommand is run

--- src/main/java/frc/acq/AcqCommand.java
+++ src/main/java/frc/acq/AcqCommand.java
@@ -3,5 +3,5 @@ package frc.acq;
 public abstract class AcqCommand {
 
     //Will be called by subsystem to execute this command
-    public abstract double execute();
+    public abstract int execute();
 }
---
+++ src/main/java/frc/acq/commands/AcqRollers.java
@@ -0,0 +1,11 @@
+package frc.acq.commands;
+
+import frc.acq.AcqCommand;
+
+public class AcqRollers extends AcqCommand{
+
+	@Override
+	public int execute() {
+			return 0;
+	}
+}

6fb4040be334b3a77cd194d1fba030486e944085
JTMongeon <jtmongeon@gmail.com>
2020-02-13 18:20:12 -0500

    should work

--- src/main/java/frc/controllers/TeleopControls.java
+++ src/main/java/frc/controllers/TeleopControls.java
@@ -1,5 +1,6 @@
 package frc.controllers;
 
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.Joystick;
 import frc.subsystem.Acquisitions;
 import frc.subsystem.Drives;
@@ -12,10 +13,10 @@ public class TeleopControls extends Controller {
     private Axis axis2;
 	
 	private Button moveForwardButton;
-	private Button backwardButton;
+	private Button limeLightButton;
 	private Button moveRight;
 	private Button buttonLeft;
-	
+	boolean isOn = true;
 	/**
 	 * Constructor - created by SubsystemManager.java
 	 * @param drives - Drives Subsystem
@@ -29,26 +30,36 @@ public class TeleopControls extends Controller {
         axis2 = new Axis(joystick, 1, true);
         
         moveForwardButton = new Button(joystick, 1);
-        backwardButton = new Button(joystick, 2);
+       limeLightButton = new Button(joystick, 2);
         moveRight = new Button(joystick, 4);
 		buttonLeft = new Button(joystick, 3);
+	
 		
     }
 
     @Override
     public void execute() {
-    	drives.setJoysticks(axis1.get(), axis2.get());
-    	if(backwardButton.get()) {
-    		drives.moveBackward(5);
+		System.out.println("testing: "+isOn);
+		try{Thread.sleep(100);}
+		catch(Exception e){}
+			
+		if(limeLightButton.get()) {
+
+			if(isOn){
+				NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(1);
+			}else{
+				NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(3);
+			}
+    		isOn = !isOn;
     	}
-    	if(moveForwardButton.get()) {
-    		drives.moveForward(5); //in feet
-    	}
-    	if(moveRight.get()) {
-    		drives.turnRight(90);
-    	}
-    	if(buttonLeft.get()) {
-			drives.turnLeft(90);
-		}
+    	// if(moveForwardButton.get()) {
+    	// 	drives.moveForward(5); //in feet
+    	// }
+    	// if(moveRight.get()) {
+    	// 	drives.turnRight(90);
+    	// }
+    	// if(buttonLeft.get()) {
+		// 	drives.turnLeft(90);
+		// }
     } 
 }

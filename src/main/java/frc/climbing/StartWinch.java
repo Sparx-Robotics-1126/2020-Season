936f3b40ec2cb5c81c7d55147810668a130c4dab
Danny Gramowski <dgramows@gmail.com>
2020-02-14 17:38:53 -0500

    need to finish Junit testing

---
+++ src/main/java/frc/climbing/StartWinch.java
@@ -0,0 +1,21 @@
+package frc.climbing;
+
+public class StartWinch extends ClimbingCommand {
+	private double wantedDistance;
+	private double currentDistance;
+
+
+	public StartWinch(ClimbingSensorsInterface sensors,double distance) {
+		super(sensors);
+		this.currentDistance = sensors.getWinchDistance();
+		this.wantedDistance = distance;
+	}
+
+		public ClimbingOutput execute() {
+			if(currentDistance >= wantedDistance) {
+				return new ClimbingOutput(0.0,true);
+			}
+			return new ClimbingOutput(0.3,false);
+		}
+
+}

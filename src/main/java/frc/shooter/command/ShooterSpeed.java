3d4e75d57d8255e9c397c363ae20268d78f485e3
Edward <edward.boone2@aol.com>
2020-02-14 18:58:47 -0500

    made a pid, needs to be tuned

--- src/main/java/frc/shooter/command/ShooterSpeed.java
+++ src/main/java/frc/shooter/command/ShooterSpeed.java
@@ -1,5 +1,6 @@
 package frc.shooter.command;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.drives.DrivesSensorInterface;
 import frc.shooter.ShooterCommand;
 import frc.shooter.ShooterOutput;
@@ -7,27 +8,36 @@ import frc.shooter.ShooterSensorsInterfeace;
 import frc.shooter.MathHelpers;
 
 public class ShooterSpeed extends ShooterCommand {
-	double speedVar = 1; //final power inputed into output; manipulated every time execute is called to reach desired rps
-	// double previousRps; //set to currentRps before return to be used in the next execute
-	int isReady = 0; //used to make sure motor speed is stable; runs test 8 times and resets if power is outside desired range
-	// final double maxIncrease = 1.02; //used to quickly increase power to motors
-	// final double minIncrease = 1.005; //used to gradually increase power to motors
-	// final double maxDecrease = 0.98; //used to quickly decrease power to motors
-	// final double midDecrease = 0.99; //used to decrease power to motors
-	// final double minDecrease = 0.995; //used to gradually decrease power to motors
-	double p = .01;
+	double errorPrior;
+	double integralPrior;
+
+	double kP = 1;
+	double kI = 0;
+	double kD = 0;
+
+	double acceptableError = 1;
 
 	public ShooterSpeed(ShooterSensorsInterfeace sensors, DrivesSensorInterface driveSensors) {
 		super(sensors, driveSensors);
 	}
 	
-	public ShooterOutput execute() {
-		double speed = sensors.getShooterSpeed();
-		double wantedSpeed = MathHelpers.getShootingSpeed(sensors.getDistanceToTarget());
-		speedVar += (speed-wantedSpeed)*p;
-		if(speedVar>1){
-			speedVar = 1;
-		}
-		return new ShooterOutput(speedVar, false); //returns edited speedVar to the power output
+	public ShooterOutput execute() { 
+		double distance = sensors.getDistanceToTarget();
+		double wantedSpeed = MathHelpers.getShootingSpeed(distance); //Calc wanted speed from are distance 
+		double actualSpeed = sensors.getShooterSpeed();
+		
+		double error =  wantedSpeed - actualSpeed;
+		double integral = integralPrior + error;
+		double derivative = (error-errorPrior);
+		double output = kP*error+kI*integral+kD*derivative;
+
+		errorPrior = error;
+		integralPrior = integral;
+
+		SmartDashboard.putNumber("Actual Speed", actualSpeed);
+		SmartDashboard.putNumber("Wanted Speed", wantedSpeed);
+
+		//returns output from pid loop, and true/false if we are close enough to wanted speed
+		return new ShooterOutput(output, (Math.abs(actualSpeed-wantedSpeed)>acceptableError)); 
 	}
 }

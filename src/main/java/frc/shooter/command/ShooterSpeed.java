1e8826fa4f1da8b6d333596e193c79961ae2b003
stephen318 <56097819+stephen318@users.noreply.github.com>
2020-02-15 12:12:28 -0500

    Commit failed

--- src/main/java/frc/shooter/command/ShooterSpeed.java
+++ src/main/java/frc/shooter/command/ShooterSpeed.java
@@ -7,84 +7,74 @@ import frc.shooter.ShooterSensorsInterfeace;
 import frc.shooter.MathHelpers;
 
 public class ShooterSpeed extends ShooterCommand {
-	double speedVar = 1; //final power inputed into output; manipulated every time execute is called to reach desired rps
-	double previousRps; //set to currentRps before return to be used in the next execute
-	int isReady = 0; //used to make sure motor speed is stable; runs test 8 times and resets if power is outside desired range
-	final double maxIncrease = 1.02; //used to quickly increase power to motors
-	final double minIncrease = 1.005; //used to gradually increase power to motors
-	final double maxDecrease = 0.98; //used to quickly decrease power to motors
-	final double midDecrease = 0.99; //used to decrease power to motors
-	final double minDecrease = 0.995; //used to gradually decrease power to motors
+	private double currentRps;
+	private double desiredRps;
+	private double outputValue = 0.5;
+	private boolean readyToShoot = false;
+	private int isReady = 0; //used to make sure motor speed is stable; runs test 8 times and resets if power is outside desired range
+	private final double FACTOR = 0.2; //change this to increase speed that the power responds; starts at 20%
+	private final double MIN_POWER_OUTPUT = 0.1; //used to make sure function does not output too low of a power value and take a long time to increase.
+	private final double TARGET_RANGE_OFFSET = 0.02; //allows for the motor to be this amount off while still returning true
+	private final int CONFIRM_STABILITY_COUNT = 5; //returns false this many times while in target range to ensure speed is stable
 	public ShooterSpeed(ShooterSensorsInterfeace sensors, DrivesSensorInterface driveSensors) {
 		super(sensors, driveSensors);
 	}
 	
-	public ShooterOutput execute() {
-			
-		double desiredRps = MathHelpers.getShootingSpeed(sensors.getDistanceToTarget());
-		double currentRps = sensors.getShooterSpeed();
+	public ShooterOutput execute() { //will be called every 5 milliseconds; requires speed from 0 to 1 and a boolean for if it is ready to shoot
 		
+		// Read from LimeLight, convert to desired RPS
+		this.desiredRps = MathHelpers.getShootingSpeed(sensors.getDistanceToTarget());
+		this.currentRps = sensors.getShooterSpeed();
+		double speedRatio; 
+		this.readyToShoot = false;
 		
-		if (currentRps <= 0.95 * desiredRps) { //if speed is less than 95% of desired speed, give motors full power
-			previousRps = currentRps;
-			return new ShooterOutput(1, false); //first number is power from 0 - 1, false is boolean for whether or not it is ready to shoot
+		if (this.desiredRps == 0) { //returns output of 0 when desired is 0 to prevent divide by 0 errors
+			this.outputValue = 0;
+			return new ShooterOutput(this.outputValue, false); //false means not ready to shoot
 		}
-		else if (currentRps <= 0.98 * desiredRps) { //rps between 95% and 98% of target value
-			if (previousRps < currentRps) { //check to make sure speed is increasing
-				speedVar = speedVar * minDecrease; //change speedVar and skip to bottom; applies to every case without return statement
-				isReady = 0; //resets number of tries when rps leaves target range
+		//changes motor speed faster while further away from desired and slower while closer
+		if (isBelowTarget()) { //if the robot should accelerate 
+			//calculate difference between desired and current over desired; output is larger based on how far apart the numbers are
+			speedRatio = (this.desiredRps - this.currentRps)/this.desiredRps;
+			this.outputValue += this.outputValue * (FACTOR * speedRatio); //changes speed by calculated value times change factor
+		}
+		else {
+			speedRatio = (this.currentRps - this.desiredRps)/this.currentRps; //does similar function as above code, but decelerates
+			this.outputValue -= this.outputValue * (FACTOR * speedRatio);
+		}
+		
+		if ((this.currentRps >= (1 - TARGET_RANGE_OFFSET) * this.desiredRps)
+				&& (this.currentRps <= (1 + TARGET_RANGE_OFFSET) * this.desiredRps)) { //if within desired range
+			if (this.isReady > CONFIRM_STABILITY_COUNT) { //checks to make sure speed is stable
+				this.readyToShoot = true;
 			}
 			else {
-				if (speedVar * 1.02 > 1.0) { //makes sure returned value is not larger than 1.0
-					isReady = 0;
-					previousRps = currentRps;
-					return new ShooterOutput(1.0, false); 
-				}
-				else {
-					speedVar = speedVar * maxIncrease;
-					isReady = 0;
-				}
+				this.isReady += 1; //runs specified number of times before returning true
 			}
 		}
-		else if (currentRps >= 1.1 * desiredRps) { //if it overshoots significantly
-			if (previousRps <= currentRps) { //changes faster if still accelerating
-				speedVar = speedVar * maxDecrease;
-			}
-			else {
-				speedVar = speedVar * midDecrease;
-			}
+		else {
+			this.isReady = 0; //resets outside target range
 		}
-		else if (currentRps >= 1.02 * desiredRps) { //if it overshoots speed
-			if (previousRps <= currentRps) { //changes faster if still accelerating
-				speedVar = speedVar * midDecrease;
-				isReady = 0;
-			}
-			else {
-				speedVar = speedVar * minDecrease;
-				isReady = 0;
-			}
+		
+		if (this.outputValue > 1) {
+			this.outputValue = 1; //ensures outputted value is not greater than 1
 		}
-		else if ((currentRps >= 0.98 * desiredRps) && (currentRps <= 1.02 * desiredRps)) { //if in correct range
-			if (previousRps > currentRps) { //if decelerating, slightly increase speed
-				speedVar = speedVar * minIncrease;
-				if (isReady == 8) { //runs through 8 times to ensure it stays in the correct range
-					return new ShooterOutput(speedVar, true); //returns true for ready to shoot
-				}
-				else {
-					isReady += 1;
-				} 
-			}
-			else if (previousRps < currentRps) { //if accelerating, slightly decrease speed
-				speedVar = speedVar * minDecrease;
-				if (isReady == 8) { //runs through 8 times to ensure it stays in the correct range
-					return new ShooterOutput(speedVar, true); //returns true for ready to shoot
-				}
-				else {
-					isReady += 1;
-				}
-			}
+		if (this.outputValue < MIN_POWER_OUTPUT) {
+			this.outputValue = MIN_POWER_OUTPUT; //ensures outputted value is not less than 0
 		}
-		previousRps = currentRps; 
-		return new ShooterOutput(speedVar, false); //returns edited speedVar to the power output
+		
+		return new ShooterOutput(this.outputValue, readyToShoot);
+	}
+	
+	private boolean isBelowTarget() {
+		return (desiredRps > currentRps);
+	}
+	
+	
+	public boolean isReadyToShoot(double currentRps, double desiredRps) { //Want to be within 2% of target RPS
+		if ((currentRps >= 0.98 * desiredRps) && (currentRps <= 1.02 * desiredRps)) {
+			return true;
+		}
+		return false;
 	}
 }

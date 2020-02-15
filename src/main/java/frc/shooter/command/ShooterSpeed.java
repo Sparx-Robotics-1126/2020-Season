a9bb1c93855741122f038e142454221a00f3bbdf
skylerssparxemail@gmail.com <skylerssparxemail@gmail.com>
2020-02-15 12:04:31 -0500

    uses talon pid

--- src/main/java/frc/shooter/command/ShooterSpeed.java
+++ src/main/java/frc/shooter/command/ShooterSpeed.java
@@ -11,32 +11,50 @@ public class ShooterSpeed extends ShooterCommand {
 	double errorPrior;
 	double integralPrior;
 
-	double kP = 1;
+	double kP = .1;
 	double kI = 0;
 	double kD = 0;
+
+	double output = 1;
 
 	double acceptableError = 1;
 
 	public ShooterSpeed(ShooterSensorsInterfeace sensors, DrivesSensorInterface driveSensors) {
 		super(sensors, driveSensors);
+		SmartDashboard.putNumber("Wanted speed", 0);
+		SmartDashboard.putNumber("kP",Double.MIN_VALUE);
 	}
 	
 	public ShooterOutput execute() { 
+		SmartDashboard.getNumber("kP", .01);
+
 		double distance = sensors.getDistanceToTarget();
-		double wantedSpeed = MathHelpers.getShootingSpeed(distance); //Calc wanted speed from are distance 
+		// double wantedSpeed = MathHelpers.getShootingSpeed(distance); //Calc wanted speed from are distance 
+		double wantedSpeed = SmartDashboard.getNumber("Wanted speed", 0);
+
 		double actualSpeed = sensors.getShooterSpeed();
 		
 		double error =  wantedSpeed - actualSpeed;
 		double integral = integralPrior + error;
 		double derivative = (error-errorPrior);
-		double output = kP*error+kI*integral+kD*derivative;
+		output += kP*error+kI*integral+kD*derivative;
+
+		if(output > 1){
+			output = 1;
+		}
+		if(output < 0){
+			output = 0;
+		}
 
 		errorPrior = error;
 		integralPrior = integral;
 
-		SmartDashboard.putNumber("Actual Speed", actualSpeed);
-		SmartDashboard.putNumber("Wanted Speed", wantedSpeed);
+		// SmartDashboard.putNumber("Actual Speed", actualSpeed);
+		// SmartDashboard.putNumber("Wanted Speed", wantedSpeed);
 
+		output = wantedSpeed;
+		SmartDashboard.putNumber("Output", output);
+		
 		//returns output from pid loop, and true/false if we are close enough to wanted speed
 		return new ShooterOutput(output, (Math.abs(actualSpeed-wantedSpeed)>acceptableError)); 
 	}
--- src/main/java/frc/subsystem/Shooter.java
+++ src/main/java/frc/subsystem/Shooter.java
@@ -1,13 +1,19 @@
 package frc.subsystem;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.drives.DrivesSensorInterface;
 import frc.robot.IO;
 import frc.shooter.ShooterCommand;
 import frc.shooter.ShooterOutput;
 import frc.shooter.ShooterSensors;
 import frc.shooter.ShooterSensorsInterfeace;
+import frc.shooter.command.CenterTurretCommand;
+import frc.shooter.command.ShooterSpeed;
 import frc.shooter.commands.LimelightTurret;
 
 public class Shooter extends Subsystem{
@@ -22,28 +28,69 @@ public class Shooter extends Subsystem{
 	private TalonSRX flywheelMotorBeta;
 	private TalonSRX turretMotor;
 
-	
+	private double feed = 	0;
+	private double kP = 	0;
+	private double kI  = 	0;
+	private double kD = 	0;
+
+
 	public Shooter(DrivesSensorInterface driveSensors) {
 		this.driveSensors = driveSensors;
 		this.shooterSensors = new ShooterSensors();
-		shooterCommand = null;
-		turretCommand = null;
-		flywheelMotorAlpha = new TalonSRX(IO.SHOOTER_FLYWHEEL_1);
-		flywheelMotorBeta = new TalonSRX(IO.SHOOTER_FLYWHEEL_2);
+		shooterCommand = new ShooterSpeed(shooterSensors,driveSensors);
+		turretCommand = new CenterTurretCommand(shooterSensors, driveSensors);
+		flywheelMotorAlpha = new TalonSRX(IO.SHOOTER_FLYWHEEL_2);
+		flywheelMotorBeta = new TalonSRX(IO.SHOOTER_FLYWHEEL_1);
+		
+		SmartDashboard.putNumber("Feed",0);
+		SmartDashboard.putNumber("kP",0);
+		SmartDashboard.putNumber("kI",0);
+		SmartDashboard.putNumber("kD",0);
+		
+
+
+		flywheelMotorAlpha.configFactoryDefault();
+		flywheelMotorAlpha.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative,0,30);
+		
+		flywheelMotorAlpha.setSensorPhase(true);
+		flywheelMotorAlpha.configNominalOutputForward(0);
+		flywheelMotorAlpha.configNominalOutputReverse(0);
+		flywheelMotorAlpha.configPeakOutputForward(1);
+		flywheelMotorAlpha.configPeakOutputReverse(0);
+		
+		flywheelMotorAlpha.config_kF(0,feed,30);
+		flywheelMotorAlpha.config_kP(0,kP,30);
+		flywheelMotorAlpha.config_kI(0,kI,30);
+		flywheelMotorAlpha.config_kD(0,kD,30);
+
+		flywheelMotorBeta.set(ControlMode.Follower,flywheelMotorAlpha.getDeviceID());
+		flywheelMotorBeta.setInverted(true);
 		turretMotor = new TalonSRX(IO.SHOOTER_TURRET_MOTOR);
 	}
 	
 	@Override
 	void execute() {
-		if(shooterCommand != null) {
-			ShooterOutput shooterOutput = shooterCommand.execute();
-			ShooterOutput turretOutput = turretCommand.execute();
-			readyToShoot = shooterOutput.isReadyToShoot() && turretOutput.isReadyToShoot();
- 			flywheelMotorAlpha.set(ControlMode.PercentOutput, -shooterOutput.getOutputValue());
-			flywheelMotorBeta.set(ControlMode.PercentOutput, shooterOutput.getOutputValue());
-			turretMotor.set(ControlMode.PercentOutput, turretOutput.getOutputValue());
-		} 
+		if(shooterCommand != null ) {
+			feed = SmartDashboard.getNumber("Feed",0);
+			kP = SmartDashboard.getNumber("kP",0);
+			kI = SmartDashboard.getNumber("kI",0);
+			kD = SmartDashboard.getNumber("kD",0);
 
+			flywheelMotorAlpha.config_kF(0,feed,30);
+			flywheelMotorAlpha.config_kP(0,kP,30);
+			flywheelMotorAlpha.config_kI(0,kI,30);
+			flywheelMotorAlpha.config_kD(0,kD,30);	
+			
+
+			ShooterOutput shooterOutput = shooterCommand.execute();
+			// ShooterOutput turretOutput = turretCommand.execute();
+			// readyToShoot = shooterOutput.isReadyToShoot() && turretOutput.isReadyToShoot();
+			SmartDashboard.putNumber("Actual Speed",flywheelMotorAlpha.getSelectedSensorVelocity()*10/(1024.0));
+ 			flywheelMotorAlpha.set(ControlMode.Velocity, (1024/100.0)*shooterOutput.getOutputValue());
+			 // flywheelMotorBeta.set(ControlMode.PercentOutput, shooterOutput.getOutputValue());
+			// turretMotor.set(ControlMode.PercentOutput, turretOutput.getOutputValue());
+		} 
+		SmartDashboard.putNumber("Encoder",flywheelMotorAlpha.getSelectedSensorPosition());
 	} 
 
 

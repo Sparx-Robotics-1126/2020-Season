c05eafd5494c8373200430eb35f35d6a8ab5bd33
cheesemanAidan <cheesemanaidan@gmail.com>
2020-02-18 18:01:34 -0500

    extend scissor lift

---
+++ src/test/java/frc/climbing/commands/TestExtendScissorLift.java
@@ -0,0 +1,49 @@
+package frc.climbing.commands;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import frc.climbing.ClimbingOutput;
+import frc.climbing.ClimbingSensorsInterface;
+import frc.climbing.TestClimbingSensors;
+import frc.climbing.commands.ExtendScissorLift;
+
+public class TestExtendScissorLift {
+	
+	
+	@Test
+	public void underBar() {
+		
+		TestClimbingSensors sensor = new TestClimbingSensors();
+		sensor.leadScrewDistance = 1;
+		sensor.isTouchingBar = false;
+		ExtendScissorLift extending = new ExtendScissorLift(sensor, 40);
+		ClimbingOutput output = extending.execute();
+		assertEquals(output.getOutput(), 0.5,0);
+	}
+	
+	@Test
+	public void atBar() {
+		TestClimbingSensors sensor = new TestClimbingSensors();
+		sensor.leadScrewDistance = 4;
+		sensor.isTouchingBar = true;
+		ExtendScissorLift extending = new ExtendScissorLift(sensor, 40);
+		ClimbingOutput output = extending.execute();
+		assertEquals(output.getOutput(), 0.5,0);
+
+	}
+	
+	@Test
+	public void overBar() {
+		TestClimbingSensors sensor = new TestClimbingSensors();
+		sensor.leadScrewDistance = 44;
+		sensor.isTouchingBar = false;
+		ExtendScissorLift extending = new ExtendScissorLift(sensor, 40);
+		ClimbingOutput output = extending.execute();
+		assertEquals(output.getOutput(), 0.5,0);
+
+	}
+	
+	
+
+}

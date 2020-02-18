f309fc1fa1e058c9511507d3ae04da4a74079275
skylerssparxemail@gmail.com <skylerssparxemail@gmail.com>
2020-02-18 16:26:31 -0500

    finished testing

--- src/main/java/frc/storage/command/IndexBalls.java
+++ src/main/java/frc/storage/command/IndexBalls.java
@@ -1,6 +1,5 @@
 package frc.storage.command;
 
-import frc.shooter.ShooterOutput;
 import frc.storage.StorageCommand;
 import frc.storage.StorageOutput;
 import frc.storage.StorageSensorInterface;
@@ -14,11 +13,18 @@ public class IndexBalls extends StorageCommand {
     boolean indexRollers;
 
     boolean pastIntakeSensorValue;
+    boolean pastShootingSensorValue;
+    
 
     public IndexBalls(StorageSensorInterface sensors, short amountOfBalls) {
         super(sensors);
         this.amountOfBalls = amountOfBalls;
         pastIntakeSensorValue = sensors.getIntakeSensor();
+        pastShootingSensorValue = sensors.getShootSensor();
+        if(pastIntakeSensorValue)
+            amountOfBalls++;
+        if(pastShootingSensorValue)
+            amountOfBalls--;
     }
 
     @Override
@@ -30,11 +36,22 @@ public class IndexBalls extends StorageCommand {
             pastIntakeSensorValue = false;
         }
 
+        if(!pastShootingSensorValue && sensors.getShootSensor()) {
+            pastShootingSensorValue = true;
+            amountOfBalls -= 1;
+        }else if(pastShootingSensorValue & !sensors.getShootSensor()){
+            pastShootingSensorValue = false;
+        }
+
         if(sensors.getIndexSensor() && !sensors.getShootSensor()){
             return new StorageOutput(.2, amountOfBalls);
         }
 
+        if(amountOfBalls<0){
+            amountOfBalls = 0;
+        }
+
         return new StorageOutput(0, amountOfBalls);
     }
-    
+
 }
---
+++ src/test/java/frc/storage/IndexBallsTest.java
@@ -0,0 +1,75 @@
+package frc.storage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import frc.storage.command.IndexBalls;
+
+
+public class IndexBallsTest {
+   
+    @Test
+    public void testBallEntering(){
+        StorageTestSensor tsa = new StorageTestSensor();
+        IndexBalls dexter = new IndexBalls(tsa,(short)0);
+
+        tsa.testShootSensor = false;
+        tsa.testIntakeSensor = true; 
+        tsa.testIndexSensor  = false;
+        StorageOutput output = dexter.execute();
+        assertEquals(output.getPrimaryOutput(),0,0);
+        assertEquals(output.getNumOfBallsAquired(),1);
+    }
+
+    @Test 
+    public void shootingBall(){
+        StorageTestSensor tsa = new StorageTestSensor();
+        IndexBalls dexter = new IndexBalls(tsa,(short)1);
+
+        tsa.testIntakeSensor = false; 
+        tsa.testIndexSensor  = false;
+        tsa.testShootSensor  = true;
+        StorageOutput output = dexter.execute();
+        assertEquals(output.getPrimaryOutput(),0,0);
+        assertEquals(output.getNumOfBallsAquired(),0);    
+    } 
+   
+    @Test
+    public void testingEverything(){
+        StorageTestSensor tsa = new StorageTestSensor();
+        IndexBalls dexter = new IndexBalls(tsa,(short)0);
+
+        tsa.testIntakeSensor = true; 
+        tsa.testIndexSensor  = false;
+        tsa.testShootSensor  = false;
+        StorageOutput output = dexter.execute();
+        assertEquals(output.getPrimaryOutput(),0,0);
+        assertEquals(output.getNumOfBallsAquired(),1);
+
+        tsa.testIntakeSensor = false; 
+        tsa.testIndexSensor  = true;
+        output = dexter.execute();
+        assertEquals(output.getPrimaryOutput(),.2,0);
+        assertEquals(output.getNumOfBallsAquired(),1,0);
+
+        tsa.testIntakeSensor = true;
+        tsa.testIndexSensor = true;
+        output = dexter.execute();
+        assertEquals(output.getPrimaryOutput(),.2,0);
+        assertEquals(output.getNumOfBallsAquired(),2,0);
+
+        tsa.testIntakeSensor = false;
+        tsa.testIndexSensor = false;
+        output = dexter.execute();
+        assertEquals(output.getPrimaryOutput(),0,0);
+        assertEquals(output.getNumOfBallsAquired(),2,0);
+
+        tsa.testShootSensor = true;
+        output = dexter.execute();
+        assertEquals(output.getPrimaryOutput(),0,0);
+        assertEquals(output.getNumOfBallsAquired(),1,0);
+        
+
+    }
+}

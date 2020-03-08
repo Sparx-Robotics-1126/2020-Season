9a3069753aeb70046af7d841a5b64d081a764d83
skylerssparxemail@gmail.com <skylerssparxemail@gmail.com>
2020-03-07 22:56:12 -0500

    Changed equation for new compression

--- src/main/java/frc/shooter/MathHelpers.java
+++ src/main/java/frc/shooter/MathHelpers.java
@@ -10,7 +10,11 @@ public class MathHelpers {
 	}
 	
 	public static double getShootingSpeed(double distanceFromTarget) {
-		return 79.5-.173*distanceFromTarget+5.4*Math.pow(10,-4)*Math.pow(distanceFromTarget,2); //Using trendline
+		if(distanceFromTarget<200){
+			return 80.19593-0.1052412*distanceFromTarget+2.74896*Math.pow(10,-4)*Math.pow(distanceFromTarget,2);
+		}
+		return 239.24833-2.0422343*distanceFromTarget+0.0060037235*Math.pow(distanceFromTarget,2);
+		// return 79.5-.173*distanceFromTarget+5.4*Math.pow(10,-4)*Math.pow(distanceFromTarget,2); //Using trendline
 		// return Math.sqrt((distanceFromTarget*distanceFromTarget*9.8)/(2*Math.cos(35)*heightOfPort-Math.tan(35))); //Using math
 	}
 
